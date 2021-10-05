ac905847db5251db288d3ed0fa97d1f0a2540f54
Essien Bassey <basseyalison@gmail.com>
2021-10-05 08:44:55 -0500

    Adding Elements and colors

--- App.js
+++ App.js
@@ -2,10 +2,27 @@
 (function() {
   "use strict";
 
-  // Start here
-  var ProductCustomizer = React.createElement('div', {className: "customizer" }, "Product customizer will go here" );
+  function ProductImage(props) {
+    return React.createElement("img", {
+      src: "../../../assets/red.jpg",
+      alt: "Product Image"
+    });
+  }
 
-  ReactDOM.render(ProductCustomizer, document.getElementById('react-root'));
+  function ProductCustomizer(props) {
+    return React.createElement(
+      "div",
+      { className: "customizer" },
+      React.createElement(
+        "div",
+        { className: "product-image" },
+        React.createElement(ProductImage)
+      )
+    );
+  }
 
+  ReactDOM.render(
+    React.createElement(ProductCustomizer),
+    document.getElementById("react-root")
+  );
 })();
-
